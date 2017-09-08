7daedb9a897adfded97b6a6f8b8b55e36891e57e
Alex Dubrovsky <ad.12379@gmail.com>
2017-09-09 00:45:40 +0300

    test

--- russian_peasant.py
+++ russian_peasant.py
@@ -1,13 +1,36 @@
+import time
+
+
+CACHE = {}
+
+
 def rus_peasant(a, b):
-    res = 0
-    while a > 0:
-        if a % 2 != 0:
-            res += b
-        a >>= 1
-        b <<= 1
+    key = (a, b)
+    if key in CACHE:
+        res = CACHE[key]
+    else:
+        res = 0
+        while a > 0:
+            if a % 2 != 0:
+                res += b
+            a >>= 1
+            b <<= 1
     return res
 
-num1 = 13
-num2 = 238
-print rus_peasant(num1, num2)
-print '{} * {} = {}'.format(num1, num2, num1*num2)
+
+def test_russian(a, b):
+    assert rus_peasant(a, b) == a * b
+    start_time = time.time()
+    print start_time
+    print rus_peasant(a, b)
+    print time.time()
+    print 'The algorithm took %f seconds' % (time.time() - start_time)
+
+
+test_russian(1234374578578678962341234, 1234122454588956562341234341234)
+# test_russian(13, 238)
+
+# num1 = 13
+# num2 = 238
+# print rus_peasant(num1, num2)
+# print '{} * {} = {}'.format(num1, num2, num1*num2)
